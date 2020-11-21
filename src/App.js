886df19bb4d6da47298c8e576de599e58ed51881
hyecheon <rainbow880616@gmail.com>
2020-11-21 23:11:00 +0900

    BooleanComponent

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import './App.css';
 import React from "react";
-import ChildComponent from "./03/ChildComponent";
+import BooleanComponent from "./03/BooleanComponent";
 
 function App() {
-  const array = [1, 2, 3];
-  const obj = {name: "제목", age: 30};
-  const node = <h1>노드</h1>;
-  const func = () => {
-    console.log("메시지");
-  };
+
   return (
-      <ChildComponent
-          boolValue={true}
-          numValue={1}
-          arrayValue={array}
-          objValue={obj}
-          nodeValue={node}
-          funcValue={func}
-      />
+      <div>
+        <div><b>지루할 때:<BooleanComponent bored/></b></div>
+        <div><b>지루할 때:<BooleanComponent/></b></div>
+      </div>
   );
 }
 
